a7b5052718bb16d481a068af2dfd89ba6927861b
Andrey <ham2188@mail.ru>
2025-09-03 21:46:39 +0300

    Интеграция Telegram API [#505295]

--- src/main/java/ru/job4j/bmb/Main.java
+++ src/main/java/ru/job4j/bmb/Main.java
@@ -5,8 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import ru.job4j.bmb.content.Content;
 import ru.job4j.bmb.services.TelegramBotService;
+import ru.job4j.bmb.services.TgRemoteService;
 
 @SpringBootApplication
 public class Main {
@@ -26,6 +30,20 @@ public class Main {
     public CommandLineRunner checEnvk(ApplicationContext ctx) {
         return args -> {
             System.out.println(ctx.getEnvironment().getProperty("telegram.bot.name"));
+        };
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            var bot = ctx.getBean(TgRemoteService.class);
+            var botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            try {
+                botsApi.registerBot(bot);
+                System.out.println("Бот успешно зерегестрирован");
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
         };
     }
 }
