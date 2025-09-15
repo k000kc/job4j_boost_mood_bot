8883b6d10b79c0cfe48e29f158fa2ef63043c196
Andrey <ham2188@mail.ru>
2025-09-15 21:39:10 +0300

    Механизм рассылки уведомлений [#505297]

--- src/main/java/ru/job4j/bmb/services/ReminderService.java
+++ src/main/java/ru/job4j/bmb/services/ReminderService.java
@@ -3,12 +3,32 @@ package ru.job4j.bmb.services;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import ru.job4j.bmb.repository.UserRepository;
 
 @Service
 public class ReminderService implements BeanNameAware {
 
-    private String beanName;
+    private final TgRemoteService tgRemoteService;
+
+    private final UserRepository userRepository;
+
+    public ReminderService(TgRemoteService tgRemoteService, UserRepository userRepository) {
+        this.tgRemoteService = tgRemoteService;
+        this.userRepository = userRepository;
+    }
+
+    @Scheduled(fixedRateString = "${remind.period}")
+    public void ping() {
+        for (var user : userRepository.findAll()) {
+            var message = new SendMessage();
+            message.setChatId(user.getChatId());
+            message.setText("Ping");
+            tgRemoteService.send(message);
+        }
+    }
 
     @PostConstruct
     public void init() {
@@ -22,7 +42,5 @@ public class ReminderService implements BeanNameAware {
 
     @Override
     public void setBeanName(String name) {
-        this.beanName = name;
-        System.out.println(beanName);
     }
 }
--- src/main/java/ru/job4j/bmb/services/TgRemoteService.java
+++ src/main/java/ru/job4j/bmb/services/TgRemoteService.java
@@ -8,6 +8,8 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.job4j.bmb.model.User;
+import ru.job4j.bmb.repository.UserRepository;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,6 +22,8 @@ public class TgRemoteService extends TelegramLongPollingBot {
     private final String botName;
     private final String botToken;
 
+    private final UserRepository userRepository;
+
     private static final Map<String, String> MOOD_RESP = new HashMap<>();
 
     static {
@@ -31,9 +35,11 @@ public class TgRemoteService extends TelegramLongPollingBot {
     }
 
     public TgRemoteService(@Value("${telegram.bot.name}") String botName,
-                           @Value("${telegram.bot.token}") String botToken) {
+                           @Value("${telegram.bot.token}") String botToken,
+                           UserRepository userRepository) {
         this.botName = botName;
         this.botToken = botToken;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -46,7 +52,7 @@ public class TgRemoteService extends TelegramLongPollingBot {
         return botName;
     }
 
-    private void send(SendMessage message) {
+    public void send(SendMessage message) {
         try {
             execute(message);
         } catch (TelegramApiException e) {
@@ -89,8 +95,15 @@ public class TgRemoteService extends TelegramLongPollingBot {
             send(new SendMessage(String.valueOf(chatId), MOOD_RESP.get(data)));
         }
         if (update.hasMessage() && update.getMessage().hasText()) {
-            long chatId = update.getMessage().getChatId();
-            send(sendButtons(chatId));
+            var message = update.getMessage();
+            if ("/start".equals(message.getText())) {
+                long chatId = message.getChatId();
+                var user = new User();
+                user.setClientId(message.getFrom().getId());
+                user.setChatId(chatId);
+                userRepository.save(user);
+                send(sendButtons(chatId));
+            }
         }
     }
 }
