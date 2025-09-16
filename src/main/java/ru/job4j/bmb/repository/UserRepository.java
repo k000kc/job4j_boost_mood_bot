e5a66752ccdf625997dcbb40f82db3d9b03c3b45
Andrey <ham2188@mail.ru>
2025-09-16 20:51:58 +0300

    Spring Data JPA [#505298]

--- src/main/java/ru/job4j/bmb/repository/UserRepository.java
+++ src/main/java/ru/job4j/bmb/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package ru.job4j.bmb.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.job4j.bmb.model.User;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository {
     List<User> findAll();
 
