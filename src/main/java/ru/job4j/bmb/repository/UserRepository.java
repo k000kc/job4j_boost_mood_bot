774d2693abe3beda0346ccf4b9abd7c326da6697
Andrey <ham2188@mail.ru>
2025-09-17 20:49:14 +0300

    Spring Data JPA [#505298] - fix

--- src/main/java/ru/job4j/bmb/repository/UserRepository.java
+++ src/main/java/ru/job4j/bmb/repository/UserRepository.java
@@ -1,15 +1,14 @@
 package ru.job4j.bmb.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.job4j.bmb.model.User;
 
 import java.util.List;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends CrudRepository<User, Long> {
     List<User> findAll();
 
     User findByClientId(Long clientId);
-
-    void save(User user);
 }
