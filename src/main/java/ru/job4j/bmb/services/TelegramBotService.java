30d8c176e71bcc471caf8ff719dbe81b28a269d7
Andrey <ham2188@mail.ru>
2025-10-21 21:05:10 +0300

    @Service и @Repository | BotCommandHandler - обработка комманд [#505306] - fix

--- src/main/java/ru/job4j/bmb/services/TelegramBotService.java
+++ src/main/java/ru/job4j/bmb/services/TelegramBotService.java
@@ -1,34 +1,12 @@
 package ru.job4j.bmb.services;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Service;
-import ru.job4j.bmb.content.Content;
 
 @Service
 public class TelegramBotService implements BeanNameAware {
 
     private String beanName;
-    private final BotCommandHandler handler;
-
-    public TelegramBotService(BotCommandHandler handler) {
-        this.handler = handler;
-    }
-
-    @PostConstruct
-    public void init() {
-        System.out.println("TelegramBotService is going through init.");
-    }
-
-    public void recive(Content content) {
-        handler.receive(content);
-    }
-
-    @PreDestroy
-    public void destroy() {
-        System.out.println("TelegramBotService will be destroyed now");
-    }
 
     @Override
     public void setBeanName(String name) {
